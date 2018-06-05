2bc6f4e80ac240a3cb158bd16a74d3e98d7726bc
Matthieu Godin <matthieugodin@live.fr>
2018-06-05 16:21:27 +0200

    Gestion d'upload de fichier pour les slides

--- app/Resources/assets/js/admin/translations.js
+++ app/Resources/assets/js/admin/translations.js
@@ -4,8 +4,9 @@ I18n.setTranslations({
   fr: {
     'Page not found': 'Page inexistante',
     'Route already exists': 'Ce nom ou cette adresse est déjà utilisé, veuillez utiliser autre chose.',
-    'News not found': 'Cette nouveauté n\'existe pas'
-  } 
+    'News not found': 'Cette nouveauté n\'existe pas',
+    'File Exists': 'Le fichier existe déjà'
+  }
 })
 
 I18n.setLocale('fr')
