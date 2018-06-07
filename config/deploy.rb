e1b14a1edbf165cf17fe6bd11a2d5f15ab2bc5ea
lowki <loic.bajard@gmail.com>
2018-06-07 16:48:42 +0200

    Mise à jour du deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,7 +13,7 @@ set :user, 'root'
 set :forward_agent, true
 set :term_mode, nil
 
-set :deploy_to, "/home/#{account}/nsi_prod"
+set :deploy_to, "/home/#{account}/nsi_staging"
 
 set :shared_dirs, ['vendor', 'web/uploads', 'var/cache', 'var/logs', 'node_modules']
 set :shared_files, ['web/.htaccess', 'web/robots.txt', 'app/config/parameters.yml']
@@ -25,10 +25,10 @@ task :deploy => :environment do
 		invoke :'deploy:link_shared_paths'
 		on :launch do
 			command "export SYMFONY_ENV=prod"
-			command "ea-php70 composer.phar install --optimize-autoloader"
+			command "ea-php72 composer.phar install --optimize-autoloader"
 			command "yarn install"
-			command "ea-php70 bin/console doctrine:schema:update --force"
-			command "ea-php70 bin/console assets:install --env=prod"
+			command "ea-php72 bin/console doctrine:schema:update --force"
+			command "ea-php72 bin/console assets:install --env=prod"
 			command "chmod +x ./node_modules/.bin/encore"
 			command "./node_modules/.bin/encore production"
 			command "find #{fetch(:deploy_to)} -type d -exec chmod 755 {} +"
