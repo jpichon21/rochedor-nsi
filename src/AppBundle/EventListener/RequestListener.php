c857511b3ce22b27eac29bf74d40a057f5767b0d
lowki <loic.bajard@gmail.com>
2018-06-07 16:26:18 +0200

    Fix problème de requestlistener

--- src/AppBundle/EventListener/RequestListener.php
+++ src/AppBundle/EventListener/RequestListener.php
@@ -20,6 +20,6 @@ class RequestListener
 
     public function onKernelRequest(GetResponseEvent $event)
     {
-        $this->doctrine->getConnection()->exec('PRAGMA foreign_keys = ON');
+        // $this->doctrine->getConnection()->exec('PRAGMA foreign_keys = ON');
     }
 }
