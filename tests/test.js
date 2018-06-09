567b466340d1bd2b536500ab0436aed61ee1d584
lowki <loic.bajard@gmail.com>
2018-06-09 02:02:09 +0200

    wip tests screenshots

---
+++ tests/test.js
@@ -0,0 +1,43 @@
+const Nightmare = require('nightmare')
+const assert = require('assert')
+const chai = require('chai')
+const should = chai.should()
+
+const imgDir = './tests/reports/screenshots/'
+const baseUrl = 'https://staging.rochedor.fr'
+const viewports = [
+  {width: 360, height: 640},
+  {width: 1366, height: 768},
+  {width: 1920, height: 1080}
+]
+const pages = [
+  {'name': 'home-fr', 'url': '/fr'},
+  {'name': 'home-en', 'url': '/en'},
+  {'name': 'home-es', 'url': '/es'},
+  {'name': 'home-de', 'url': '/de'},
+  {'name': 'home-it', 'url': '/it'}
+]
+viewports.forEach((viewport) => {
+  describe(`Captures ${viewport.width}x${viewport.height}`, function() {
+    let nightmare = null
+    beforeEach(() => {
+      nightmare = new Nightmare({waitTimeout: 2000})
+      nightmare.viewport(viewport.width, viewport.height)
+    })
+    pages.forEach((page) => {
+      this.timeout('30s')
+      const url = `${baseUrl}${page.url}`
+      describe(`${page.name} ${url}`, () => {
+        it(`should load ${url} without error`, done => {
+          nightmare
+            .goto(url)
+            .wait('.logo')
+            .screenshot(`${imgDir}${page.name}-${viewport.width}x${viewport.height}.png`)
+            .end()
+            .then(result => { done()})
+            .catch(done)
+          })
+        })
+      })
+    })
+  })
