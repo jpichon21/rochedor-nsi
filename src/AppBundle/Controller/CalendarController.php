710e3bd2ce699f408cc655075dfdc46195c1814d
Tristan Cuenot <lpmitristancuenot@gmail.com>
2018-06-13 17:30:09 +0200

    WIP inscription individuel

--- src/AppBundle/Controller/CalendarController.php
+++ src/AppBundle/Controller/CalendarController.php
@@ -11,6 +11,10 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use AppBundle\Repository\CalendarRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use AppBundle\Entity\Contact;
+use AppBundle\Entity\CalL;
 
 class CalendarController extends Controller
 {
@@ -30,5 +34,38 @@ class CalendarController extends Controller
         dump($sites);
         exit;
         return new Response('test');
+    }
+
+    // $contact = new Contact;
+    // $civil = $request->get('civil');
+    // $civil2 = $request->get('civil2');
+    // $nom = $request->get('nom');
+    // $prenom = $request->get('prenom');
+    // $adresse = $request->get('adresse');
+    // $cp = $request->get('cp');
+
+    public function individualInscription(Request $request, Contact $contact, $Jsco, $CodCal)
+    {
+        $contact->setJsco($Jsco);
+        $this->incrementInscr($request->get('lieu'));
+        $calL= new CalL;
+        $calL->setCodcal($CodCal);
+        $calL->getLcal($request->get('lcal'));
+        $calL->getTyplcal($request->get('typlcal'));
+        $calL->getRefLcal($request->get('reflcal'));
+        $calL->getJslcal($request->get('jslcal'));
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($calL);
+        $em->flush();
+    }
+    
+    public function incrementInscr($lieu)
+    {
+        $year = date('Y');
+        $nom = "ins".$lieu.substr($year, -2);
+        $variable = $this->getDoctrine()->getRepository('AppBundle:Variable')->findByNom($nom);
+        $variable->setValeurn($variable->getValeurn()+1);
+        $em->persist($variable);
+        $em->flush();
     }
 }
