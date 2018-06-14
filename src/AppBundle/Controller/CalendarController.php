8ac17e89172a8438455109c89652f04535ec60fe
Tristan Cuenot <lpmitristancuenot@gmail.com>
2018-06-14 08:58:02 +0200

    Ajout des persist contact et update contact

--- src/AppBundle/Controller/CalendarController.php
+++ src/AppBundle/Controller/CalendarController.php
@@ -36,13 +36,78 @@ class CalendarController extends Controller
         return new Response('test');
     }
 
-    // $contact = new Contact;
-    // $civil = $request->get('civil');
-    // $civil2 = $request->get('civil2');
-    // $nom = $request->get('nom');
-    // $prenom = $request->get('prenom');
-    // $adresse = $request->get('adresse');
-    // $cp = $request->get('cp');
+    public function updateContact(Request $request)
+    {
+        
+        $em = $this->getDoctrine()->getManager();
+        $codco = $request->get('codco');
+        $contact = $em->getRepository('AppBundle:Contact')->findOneById($codco);
+        $civil = $request->get('civil');
+        $civil2 = $request->get('civil2');
+        $nom = $request->get('nom');
+        $prenom = $request->get('prenom');
+        $adresse = $request->get('adresse');
+        $cp = $request->get('cp');
+        $ville = $request->get('ville');
+        $tel = $request->get('tel');
+        $mobil = $request->get('mobil');
+        $email = $request->get('email');
+        $profession = $request->get('profession');
+        $mpco = $request->get('mpco');
+        $datenaiss = $request->get('datnaiss');
+
+        $contact->setCivil($civil);
+        $contact->setCivil2($civil2);
+        $contact->setNom($nom);
+        $contact->setPrenom($prenom);
+        $contact->setAdresse($adresse);
+        $contact->setCp($cp);
+        $contact->setVille($ville);
+        $contact->setTel($tel);
+        $contact->setMobil($mobil);
+        $contact->setEmail($email);
+        $contact->setProfession($profession);
+        $contact->setMpco($mpco);
+        $contact->setDatenaiss($datenaiss);
+        $em->persist($contact);
+        $em->flush();
+    }
+
+    public function persistContact(Request $request)
+    {
+        
+        $contact = new Contact;
+        $em = $this->getDoctrine()->getManager();
+        $civil = $request->get('civil');
+        $civil2 = $request->get('civil2');
+        $nom = $request->get('nom');
+        $prenom = $request->get('prenom');
+        $adresse = $request->get('adresse');
+        $cp = $request->get('cp');
+        $ville = $request->get('ville');
+        $tel = $request->get('tel');
+        $mobil = $request->get('mobil');
+        $email = $request->get('email');
+        $profession = $request->get('profession');
+        $mpco = $request->get('mpco');
+        $datenaiss = $request->get('datnaiss');
+
+        $contact->setCivil($civil);
+        $contact->setCivil2($civil2);
+        $contact->setNom($nom);
+        $contact->setPrenom($prenom);
+        $contact->setAdresse($adresse);
+        $contact->setCp($cp);
+        $contact->setVille($ville);
+        $contact->setTel($tel);
+        $contact->setMobil($mobil);
+        $contact->setEmail($email);
+        $contact->setProfession($profession);
+        $contact->setMpco($mpco);
+        $contact->setDatenaiss($datenaiss);
+        $em->persist($contact);
+        $em->flush();
+    }
 
     public function individualInscription(Request $request, Contact $contact, $Jsco, $CodCal)
     {
