913a7b625f80b415bfe730b1bfa255c2e3c7c626
lowki <loic.bajard@gmail.com>
2018-06-20 17:19:10 +0200

    Ajoute les entités

---
+++ src/AppBundle/Entity/Prodrub.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Prodrub
+ *
+ * @ORM\Table(name="prodrub")
+ * @ORM\Entity
+ */
+class Prodrub
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="CodRub", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $codrub;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Rubrique", type="string", length=60, nullable=false)
+     */
+    private $rubrique;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="ImgRub", type="text", length=65535, nullable=false)
+     */
+    private $imgrub;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="LangRub", type="string", length=2, nullable=false)
+     */
+    private $langrub;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="rubHide", type="boolean", nullable=false)
+     */
+    private $rubhide;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="MemoRub", type="text", length=65535, nullable=false)
+     */
+    private $memorub;
+}
