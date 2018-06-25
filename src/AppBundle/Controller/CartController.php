9576f92b39f4dbabcbf1a21bd036699229e3f2b7
lowki <loic.bajard@gmail.com>
2018-06-25 17:51:15 +0200

    Nettoyage cartcontroller

--- src/AppBundle/Controller/CartController.php
+++ src/AppBundle/Controller/CartController.php
@@ -82,76 +82,7 @@ class CartController extends Controller
         $session->set('cart', $cart->getId());
         return $this->redirectToRoute('collection-fr', ['id' => $productId]);
     }
-
-    /**
-     * @Route("/editions-nouveautes", name="collections-news-fr")
-     * @Route("/publications-news", name="collections-news-en")
-     * @Route("/publikationen-neu", name="collections-news-de")
-     * @Route("/publicaciones-nuevo", name="collections-news-es")
-     * @Route("/pubblicazioni-nuovo", name="collections-news-it")
-     */
-    public function showNewProductsAction(Request $request)
-    {
-        $contentDocument = $this->pageService->getContentFromRequest($request);
-        $avaiableLocales = $this->pageService->getAvailableLocales($contentDocument);
-        $products = $this->productRepository->findNewProducts();
-        return $this->render(
-            'product/news.html.twig',
-            ['products' => $products, 'avaiableLocales' => $avaiableLocales, 'page' => $contentDocument]
-        );
-    }
     
-    /**
-     * @Route("/editions", name="collections-fr")
-     * @Route("/collections", name="collections-en")
-     * @Route("/publikationen", name="collections-de")
-     * @Route("/publicaciones", name="collections-es")
-     * @Route("/pubblicazioni", name="collections-it")
-     */
-    public function showCollections(Request $request)
-    {
-        $locale = $request->getLocale();
-        $collections = $this->productRepository->findCollections($locale);
-        $themes = $this->productRepository->findThemes();
-
-        $products = null;
-        
-        $reqThemes = $request->get('themes');
-        if ($reqThemes) {
-            $products = $this->productRepository->findByThemes($reqThemes);
-        }
-
-        $collection = $request->get('collection');
-        if ($collection) {
-            $products = $this->productRepository->findProducts($collection);
-        }
-
-        $productsCategorized = null;
-        if (!$products) {
-            $productsCategorized = [];
-            foreach ($collections as $collection) {
-                $productsCategorized[] = [
-                    'title' => $collection->getRubrique(),
-                    'products' => $this->productRepository->findProducts($collection->getCodrub(), 2)
-                ];
-            }
-        }
-        $contentDocument = $this->pageService->getContentFromRequest($request);
-        $avaiableLocales = $this->pageService->getAvailableLocales($contentDocument);
-
-        return $this->render(
-            'product/list.html.twig',
-            [
-                'avaiableLocales' => $avaiableLocales,
-                'page' => $contentDocument,
-                'collections' => $collections,
-                'themes' => $themes,
-                'productsCategorized' => $productsCategorized,
-                'products' => $products,
-            ]
-        );
-    }
-
     /**
      * Add a product to the Cart
      *
