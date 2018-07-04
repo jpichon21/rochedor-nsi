056e2af566fbd0f03291f3006808613928116986
Kalizo <matthieugodin@live.fr>
2018-07-04 15:13:36 +0200

    Réécriture du main sans JQuery

--- app/Resources/assets/js/main.js
+++ app/Resources/assets/js/main.js
@@ -1,53 +1,66 @@
-import $ from 'jquery'
-import Velocity from 'velocityjs'
+// Scrollbar
 
-/* Zoom Text */
+const content = document.querySelector('.content')
 
-function updateHeightDropdown () {
-  var elmt = $('.dropdown .active')[0]
-  if (elmt !== undefined) {
-    elmt.style.maxHeight = elmt.scrollHeight + 'px'
+content.style.overflowY = 'scroll'
+content.style.width = 'calc(100% + ' + (content.offsetWidth - content.clientWidth) + 'px)'
+
+// Zoom
+
+const bodyClass = document.querySelector('body').classList
+const dropdown = document.querySelector('.dropdown')
+
+const updateHeightDropdown = () => {
+  if (dropdown !== undefined) {
+    let active = dropdown.querySelector('.active')
+    active.style.maxHeight = active.scrollHeight + 'px'
   }
 }
 
-const $body = $('body')
-
-$('.zoom').on('click', '.plus', function () {
-  if ($body.hasClass('zoom-2x')) {
-    $body
-      .removeClass('zoom-2x')
-      .addClass('zoom-4x')
-  } else {
-    $body.addClass('zoom-2x')
-  }
+const handleClickZoomMinus = () => {
+  const a = 'zoom-4x'; const b = 'zoom-2x'
+  if (bodyClass.contains(a)) { bodyClass.remove(a); bodyClass.add(b) } else { if (bodyClass.contains(b)) { bodyClass.remove(b) } }
   updateHeightDropdown()
-})
+}
 
-$('.zoom').on('click', '.minus', function () {
-  if ($body.hasClass('zoom-4x')) {
-    $body
-      .removeClass('zoom-4x')
-      .addClass('zoom-2x')
-  } else {
-    $body.removeClass('zoom-2x')
-  }
+const handleClickZoomPlus = () => {
+  const a = 'zoom-2x'; const b = 'zoom-4x'
+  if (bodyClass.contains(a)) { bodyClass.remove(a); bodyClass.add(b) } else { if (!bodyClass.contains(b)) { bodyClass.add(a) } }
   updateHeightDropdown()
+}
+
+const zoomMinus = document.querySelector('.zoom .minus')
+const zoomPlus = document.querySelector('.zoom .plus')
+
+zoomMinus.onclick = () => { handleClickZoomMinus() }
+zoomPlus.onclick = () => { handleClickZoomPlus() }
+
+// Menu
+
+const menus = document.querySelectorAll('[data-menu]')
+
+const handleMenuHover = event => {
+  const reference = event.target.getAttribute('data-menu')
+  let elements = document.querySelectorAll('[data-menu="' + reference + '"]')
+  elements.forEach(element => {
+    event.type === 'mouseenter'
+      ? element.classList.add('active')
+      : element.classList.remove('active')
+  })
+}
+
+menus.forEach(menu => {
+  menu.addEventListener('mouseleave', event => handleMenuHover(event))
+  menu.addEventListener('mouseenter', event => handleMenuHover(event))
 })
 
-/* Menu */
+// Selects
 
-$('[data-menu]').on('mouseover mouseout', function (event) {
-  let reference = $(this).attr('data-menu')
-  let element = $('[data-menu="' + reference + '"]')
-  event.type === 'mouseover'
-    ? element.addClass('active')
-    : element.removeClass('active')
-})
-
-/* Selects */
-
-$('form').on('change', '.select', function () {
-  if ($(this).val() !== '') {
-    $(this).addClass('white')
+document.addEventListener('change', function (event) {
+  const element = event.target
+  if (element && element.classList.contains('select')) {
+    if (element.value !== '') {
+      element.classList.add('white')
+    }
   }
 })
