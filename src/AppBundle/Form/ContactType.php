b92b65ba00d46f4e727b7af9a39d905194f26b2c
Kalizo <matthieugodin@live.fr>
2018-07-16 17:38:23 +0200

    Correction des champs obligatoires

--- src/AppBundle/Form/ContactType.php
+++ src/AppBundle/Form/ContactType.php
@@ -15,14 +15,58 @@ class ContactType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name', TextType::class, ['attr' => ['class' => 'input', 'placeholder' => 'form.name']])
-            ->add('surname', TextType::class, ['attr' => ['class' => 'input', 'placeholder' => 'form.surname']])
-            ->add('address', TextType::class, ['attr' => ['class' => 'input', 'placeholder' => 'form.address']])
-            ->add('city', TextType::class, ['attr' => ['class' => 'input', 'placeholder' => 'form.city']])
-            ->add('zipcode', NumberType::class, ['attr' => ['class' => 'input', 'placeholder' => 'form.zipcode']])
-            ->add('email', EmailType::class, ['attr' => ['class' => 'input', 'placeholder' => 'form.email']])
-            ->add('body', TextareaType::class, ['attr' => ['class' => 'textarea', 'placeholder' => 'form.body']])
-            ->add('submit', SubmitType::class, ['attr' => ['class' => 'input button submit'], 'label' => 'form.send']);
+            ->add('name', TextType::class, [
+                'attr' => [
+                    'class' => 'input',
+                    'placeholder' => 'form.name'
+                ]
+            ])
+            ->add('surname', TextType::class, [
+                'required' => false,
+                'attr' => [
+                    'class' => 'input',
+                    'placeholder' => 'form.surname'
+                ]
+            ])
+            ->add('address', TextType::class, [
+                'required' => false,
+                'attr' => [
+                    'class' => 'input',
+                    'placeholder' => 'form.address'
+                ]
+            ])
+            ->add('city', TextType::class, [
+                'required' => false,
+                'attr' => [
+                    'class' => 'input',
+                    'placeholder' => 'form.city'
+                ]
+            ])
+            ->add('zipcode', NumberType::class, [
+                'required' => false,
+                'attr' => [
+                    'class' => 'input',
+                    'placeholder' => 'form.zipcode'
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'attr' => [
+                    'class' => 'input',
+                    'placeholder' => 'form.email'
+                ]
+            ])
+            ->add('body', TextareaType::class, [
+                'attr' => [
+                    'class' => 'textarea',
+                    'placeholder' => 'form.body'
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'form.send',
+                'attr' => [
+                    'class' => 'input button submit'
+                ]
+            ]);
     }
 
     public function getName()
