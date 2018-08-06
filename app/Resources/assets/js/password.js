7407bdb01eaf8f65684b0fd39f5d40d87b1d0873
Tristan Cuenot <lpmitristancuenot@gmail.com>
2018-08-06 11:30:33 +0200

    message des validation de mdp

--- app/Resources/assets/js/password.js
+++ app/Resources/assets/js/password.js
@@ -10,9 +10,9 @@ const second = document.getElementById('form_password_second')
 form.addEventListener('submit', event => {
   if (first.value !== second.value) {
     event.preventDefault()
-    upFlashbag(i18n.trans('security.password.must.identical'))
+    upFlashbag(i18n.trans('form.message.passwords_mismatch'))
   } else if (first.value.length < 7) {
     event.preventDefault()
-    upFlashbag(i18n.trans('security.password.must.height.char'))
+    upFlashbag(i18n.trans('validation.password.length'))
   }
 })
