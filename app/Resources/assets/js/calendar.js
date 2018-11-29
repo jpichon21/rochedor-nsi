6f077c6c8d3ffa9a296b533eaa665e6e38fb07d6
Kalizo <matthieugodin@live.fr>
2018-11-29 08:20:26 +0100

    Remaniement des <select> des Filters pour la Calendar

--- app/Resources/assets/js/calendar.js
+++ app/Resources/assets/js/calendar.js
@@ -15,10 +15,12 @@ $('.filter')
       $('.filter').removeClass('active')
       $(this).addClass('active')
     }
+    $('.content').addClass('ahead')
   })
 
   .on('mouseleave', function () {
     $(this).removeClass('active')
+    $('.content').removeClass('ahead')
   })
 
   .on('change', function () {
