5ffeba87c69c284fa404222de9f410425a3386c6
Venderlich <lpmitristancuenot@gmail.com>
2019-02-18 09:44:19 +0100

    Changer les intitulés des boutons : 'Continuer mes achats' et 'Voir mon panier'

--- app/Resources/assets/js/popup.js
+++ app/Resources/assets/js/popup.js
@@ -7,22 +7,22 @@ const buttonC = popup.querySelector('.button.continue')
 
 export const upFlashbag = html => {
   message.innerHTML = html
-  popup.classList.remove('yes', 'no', 'cart')
+  popup.classList.remove('yes', 'no', 'cart', 'continueCart')
   popup.classList.add('active', 'continue')
   buttonC.addEventListener('click', () => { downPopup() })
 }
 
 export const upCartBox = html => {
   message.innerHTML = html
-  popup.classList.remove('yes', 'no')
-  popup.classList.add('active', 'cart', 'continue')
+  popup.classList.remove('yes', 'no', 'continue')
+  popup.classList.add('active', 'cart', 'continueCart')
   buttonC.addEventListener('click', () => { downPopup() })
 }
 
 export const upConfirmbox = html => {
   return new Promise((resolve, reject) => {
     message.innerHTML = html
-    popup.classList.remove('continue', 'cart')
+    popup.classList.remove('continue', 'cart', 'continueCart')
     popup.classList.add('active', 'yes', 'no')
     buttonY.addEventListener('click', () => { downPopup(); resolve() })
     buttonN.addEventListener('click', () => { downPopup(); reject(new Error()) })
