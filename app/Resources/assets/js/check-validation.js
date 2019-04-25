620218ff04021630febe18282d08cc42ccdadb64
lowki <loic.bajard@gmail.com>
2019-04-25 13:43:14 +0200

    Supprime check-validation devenu inutile

--- app/Resources/assets/js/check-validation.js
+++
@@ -1,8 +0,0 @@
-
-const _path = document.querySelector('.path-json').innerHTML.trim()
-const _basePath = document.querySelector('.basePath-json').innerHTML.trim()
-var url = _basePath+_path
-history.pushState(null, null, location.href)
-    window.onpopstate = function () {
-        window.location.href = url
-    }
