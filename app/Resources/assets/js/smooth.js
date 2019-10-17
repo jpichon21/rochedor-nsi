7b0cd2bf963ad428dc5e751de47cf82d0c2436b3
jpichon21 <juliencfpichon@gmail.com>
2019-10-17 18:14:59 +0200

    correctif

--- app/Resources/assets/js/smooth.js
+++ app/Resources/assets/js/smooth.js
@@ -1,11 +1,17 @@
 ;(function ($) {
-    'use strict';
-    var content  = $('.m-scene').smoothState({
-          onStart : {
-            duration: 250,
-            render: function () {
-              content.toggleAnimationClass('is-exiting');
-            }
-          }
-        }).data('smoothState'); 
-  })(jQuery);
+  'use strict'
+  var content = $('.m-scene').smoothState({
+    // onStart runs as soon as link has been activated
+    onStart: {
+
+      // Set the duration of our animation
+      duration: 250,
+
+      // Alterations to the page
+      render: function () {
+        // Quickly toggles a class and restarts css animations
+        content.toggleAnimationClass('is-exiting')
+      }
+    }
+  }).data('smoothState') // makes public methods available
+})(jQuery)
