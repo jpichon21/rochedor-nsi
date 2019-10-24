838a23d360934d91614d7dded14d2756ea3cf7bd
jpichon21 <juliencfpichon@gmail.com>
2019-10-24 14:23:21 +0200

    correction boutons éditions + bug transition

--- app/Resources/assets/js/transition.js
+++ app/Resources/assets/js/transition.js
@@ -2,16 +2,22 @@ import $ from 'jquery'
 
 $(document).ready(function () {
 
-  $('.menu a, button').click(function() {
-    var $this = this
+  $('.menu a, .button').click(function() {
+    console.log("test");
+    var $this = this;
     event.preventDefault();
     var newLocation = $this.href;
-    $('body').fadeOut(1000, newpage(newLocation));
+    $('body').fadeOut(600, newpage(newLocation));
    
   });
 
     function newpage(newLocation) {
-        window.location = newLocation;
+      window.location = newLocation;
+      fadeNew();
+    }
+
+    function fadeNew(){
+      $('body').fadeIn(600);
     }
     
 });
