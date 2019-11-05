9f328027131eec1e3f497a1bf6ef3d101a9dfa0d
jpichon21 <juliencfpichon@gmail.com>
2019-11-05 17:48:01 +0100

    test

--- app/Resources/assets/js/speaker.js
+++ app/Resources/assets/js/speaker.js
@@ -24,6 +24,7 @@ $('.carousel .prev').on('click', () => {
 })
 
 $('.slide').on('click', () => {
+  var $this = $(this)
   console.log("test")
-  $(this).addClass('clicked')
+  $this.addClass('clicked')
 })
