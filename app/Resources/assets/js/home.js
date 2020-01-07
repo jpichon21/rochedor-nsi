9060c7d9bdd74adb5184083ae1519c211c79c3ed
jpichon21 <juliencfpichon@gmail.com>
2020-01-07 14:37:15 +0100

    corrige fail

--- app/Resources/assets/js/home.js
+++ app/Resources/assets/js/home.js
@@ -87,7 +87,7 @@ const changeNews = (direction = 'next') => {
   $('.nouveautes > div[data-index=' + current + ']').fadeOut(300).delay(300).removeClass('active')
   $('.nouveautes > div[data-index=' + next + ']').delay(300).fadeIn(300).addClass('active')*/
 
-)
+
   $('.nouveautes > div[data-index=' + current + ']').removeClass('active')
   $('.nouveautes > div[data-index=' + next + ']').addClass('active')
   $('.nouveautes > .active ').css({'opacity':'1','transition':'1s ease'})
