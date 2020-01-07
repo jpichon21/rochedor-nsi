ebacdbbb0d8279b8f6ce0a4e85a2c454744f6da9
jpichon21 <juliencfpichon@gmail.com>
2020-01-07 16:17:47 +0100

    [NOUVEAUTES] Test anim

--- app/Resources/assets/js/home.js
+++ app/Resources/assets/js/home.js
@@ -88,8 +88,8 @@ const changeNews = (direction = 'next') => {
   $('.nouveautes > div[data-index=' + next + ']').delay(300).fadeIn(300).addClass('active')*/
 
 
-  $('.nouveautes > div[data-index=' + current + ']').removeClass('active')
-  $('.nouveautes > div[data-index=' + next + ']').addClass('active')
+  $('.nouveautes > div[data-index=' + current + ']').removeClass('active').fadeOut(300)
+  $('.nouveautes > div[data-index=' + next + ']').addClass('active').fadeIn(300)
   //$('.nouveautes > .active ').css('opacity','1')
 
   // edge case for nav
