1231f0fa378aa4869118d6550258928064e05daa
jpichon21 <juliencfpichon@gmail.com>
2020-01-07 15:16:12 +0100

    [NOUVEAUTES] Fixtest nav

--- app/Resources/assets/js/home.js
+++ app/Resources/assets/js/home.js
@@ -90,7 +90,7 @@ const changeNews = (direction = 'next') => {
 
   $('.nouveautes > div[data-index=' + current + ']').removeClass('active')
   $('.nouveautes > div[data-index=' + next + ']').addClass('active')
-  $('.nouveautes > .active ').css('opacity','1')
+  //$('.nouveautes > .active ').css('opacity','1')
 
   // edge case for nav
 
