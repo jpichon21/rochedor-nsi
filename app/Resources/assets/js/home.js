429cd3f91b8a61e1956add78d81b75c9ec5f8002
jpichon21 <juliencfpichon@gmail.com>
2020-01-13 14:11:43 +0100

    test

--- app/Resources/assets/js/home.js
+++ app/Resources/assets/js/home.js
@@ -64,10 +64,7 @@ if (window.innerWidth <= large) {
     button.classList.remove('black')
 }
 
-if (window.innerWidth > large) {
-  button.classList.add('black')
-  button.classList.remove('yellow')
-}
+
 
 if (title !== null) {
   title.onclick = () => {
